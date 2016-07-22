792c4435907f5cddf7d30bce18d63b3320b9e012
Cameron <campoore2@gmail.com>
2016-07-22 13:37:16 -0700

    Fixed naming of a local variable in shoe_spec. All specs now passing.

--- spec/shoe_spec.rb
+++ spec/shoe_spec.rb
@@ -1,12 +1,12 @@
-# require('spec_helper')
-#
-# describe(Shoe) do
-#   it "belongs to many stores" do
-#     test_store = Store.create({:name => 'Nike'})
-#     test_shoe = Shoe.create({:name => 'Max'})
-#     test_shoe2 = Shoe.create({:name => 'Air'})
-#     test_store.shoes.push(test_shoe)
-#     test_store.shoes.push(test_shoe2)
-#     expect(test_recipe.shoes()).to(eq([test_shoe, test_shoe2]))
-#   end
-# end
+require('spec_helper')
+
+describe(Shoe) do
+  it "belongs to many stores" do
+    test_store = Store.create({:name => 'Nike'})
+    test_shoe = Shoe.create({:name => 'Max'})
+    test_shoe2 = Shoe.create({:name => 'Air'})
+    test_store.shoes.push(test_shoe)
+    test_store.shoes.push(test_shoe2)
+    expect(test_store.shoes()).to(eq([test_shoe, test_shoe2]))
+  end
+end
