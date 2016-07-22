e775d2d561cb85dcd93a3a86a93d84f685d999a1
Cameron <campoore2@gmail.com>
2016-07-22 13:35:44 -0700

    Store passes all specs.

--- spec/store_spec.rb
+++ spec/store_spec.rb
@@ -5,6 +5,7 @@ describe(Store) do
     it "tells you the shoes available" do
       test_store = Store.create({:name => "nike"})
       test_shoe = Shoe.create({:name => "max"})
+      test_store.shoes.push(test_shoe)
       expect(test_store.shoes()).to(eq([test_shoe]))
     end
   end
